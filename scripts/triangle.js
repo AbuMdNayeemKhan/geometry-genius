7bab25efe2c826ead18f3990daeb583aea87d622
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-02-12 23:59:20 +0600

    added

--- scripts/triangle.js
+++ scripts/triangle.js
@@ -1,6 +1,10 @@
 function calculateTriangleArea(){
     let triangleBaseInput = document.getElementById('triangle-base');
+    let triangleHeightInput = document.getElementById('triangle-height');
     let baseValue = triangleBaseInput.value;
+    let heihgtValue = triangleHeightInput.value;
     let vaseValueCon = parseFloat(baseValue);
-    console.log(typeof vaseValueCon);
+    let heightValueCon = parseFloat(heihgtValue);
+    console.log(vaseValueCon);
+    console.log(heightValueCon);
 }
